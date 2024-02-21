a75f0952d7e6e0d0b05354c667f3d0c3a7a7cd80
catfishfishcat10101 <catfishfishcat10101@gmail.com>
2024-02-20 19:39:12 -0600

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -240,5 +240,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 		$banner
 			._parallax();
+			
+let anims = [...document.querySelectorAll("[anim]")];
+console.log(anims);
+let click = (el, cb) => el.addEventListener("click", cb);
+let toggle = (el) => el.classList.toggle(toggled);
+let clickTog = (el) => click(el, () => toggle(el));
+anims.map(clickTog);
 
 })(jQuery);
